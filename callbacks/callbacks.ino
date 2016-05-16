f732c3a9844e70d252c31c8b67ff65ef51b9233c
Louis Quinn <louisquinn.contact@gmail.com>
2016-05-17 01:11:11 +1000

    ïmproved on-off ramps

--- callbacks/callbacks.ino
+++ callbacks/callbacks.ino
@@ -1,6 +1,10 @@
 #include <MIDI.h>
 
+#define PIN 3
+
 int pwm_val;
+int i;
+int pin_read;
 
 /* Create a MIDI instance */
 MIDI_CREATE_DEFAULT_INSTANCE();
@@ -9,20 +13,46 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 void handleNoteOn(byte channel, byte pitch, byte velocity)
 {
     pwm_val = map(velocity,0,127,0,255);
-    analogWrite(3,pwm_val);
+    pin_read = analogRead(PIN);
+
+    /* If LED state is less than velocity val: ramp up */
+    if(pin_read < pwm_val)
+    {
+        for(i = pin_read; i <= pwm_val; i++)
+        {
+            analogWrite(PIN,i);
+        }
+    }
+    /* If LED state is higher than velocity val: ramp down */
+    else if(pin_read > pwm_val)
+    {
+        for(i = pin_read; i >= pwm_val; i--)
+        {
+            analogWrite(PIN,i);
+        }
+    }
+    /* If the values are the same, write the pwm_val */
+    else if(pin_read == pwm_val)
+    {
+        analogWrite(PIN,pwm_val);
+    }
 }
 
 /* Callback for a note off */
 void handleNoteOff(byte channel, byte pitch, byte velocity)
 {
-    analogWrite(3,0);
+    /* Ramp down to zero to avoid hard transitions */
+    for(i = analogRead(PIN); i >= 0; i--)
+    {
+        analogWrite(PIN,i);
+    }
 }
 
 /* Callback for pitchbend */
 void handlePitchBend(byte channel, int bend)
 {
-    pwm_val = map(bend,0,8191,0,255);
-    analogWrite(3,pwm_val);
+    pwm_val = map(bend,0,8192,0,255);
+    analogWrite(PIN,pwm_val);
 }
 
 /* ********************************************************** */
@@ -31,7 +61,7 @@ void handlePitchBend(byte channel, int bend)
 void setup()
 {
     /* Set pin 3 for analog output */
-    pinMode(3,OUTPUT);
+    pinMode(PIN,OUTPUT);
 
     /* Register callbacks */
     MIDI.setHandleNoteOn(handleNoteOn);  
